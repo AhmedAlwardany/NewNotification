85fd7b20e75148316b5caa284bdbe6e7256693a7
Omar_ElBarbary <omar.m.elbarbary@gmail.com>
2022-06-06 22:42:37 +0200

    updated

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-const sgmail=require('@sendgrid/mail')
-const API_KEY='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';
-sgmail.setApiKey(API_KEY)
-const message={
-    to:'omar.elbarbary@student.giu-uni.de',
-    from:'hey.baraa@gmail.com',
-    subject:'Order Details',
-    text:'This is your order details'
+import express from "express";
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT || 5005;
+const app = express();
+const sendEmail = require("../notfication/sendEmail")
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+
+app.post("/notification", (req, res) => {
+
+  try{
+  const { name,email,orderId,price,adress} = req.body;
+
+  const from = "hey.baraa@gmail.com";
+  const to = email;
+
+  const subject = "Order accepted";
+
+  const output = `
+    <p>Hey ${name}, Thank You for your Order !</p>
+    <h3>Order details Details</h3>
+    <ul>
+      <li>Order ID: ${orderId}</li>
+      <li>Adress: ${adress}</li>
+      <li>Total: ${price}</li>
+    </ul>
+  `;
+
+  sendEmail(to, from, subject, output);
+  res.status(200);
+    }catch (e) {
+    res.status(400).json({message: e.message});
 }
-sgmail
-  .send(message)
-  .then((response) => console.log('Email Sent'))
-  .catch((error) => console.log(error.message));
-  
+});
